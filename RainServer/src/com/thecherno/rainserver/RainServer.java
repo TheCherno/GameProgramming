3637bd475ab8b1d6e859248b0726dd1c036b3f32
TheCherno <cherno21@hotmail.com>
2016-08-14 17:42:30 +1000

    S2E3.

--- RainServer/src/com/thecherno/rainserver/RainServer.java
+++ RainServer/src/com/thecherno/rainserver/RainServer.java
@@ -1,10 +1,22 @@
 package com.thecherno.rainserver;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class RainServer {
 
 	public static void main(String[] args) {
 		Server server = new Server(8192);
 		server.start();
+		
+		InetAddress address = null;
+		try {
+			address = InetAddress.getByName("192.168.1.10");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		int port = 8192;
+		server.send(new byte[] { 0, 1, 2 }, address, port);
 	}
 
 }
--- RainServer/src/com/thecherno/rainserver/Server.java
+++ RainServer/src/com/thecherno/rainserver/Server.java
@@ -1,18 +1,30 @@
 package com.thecherno.rainserver;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
 public class Server {
 
 	private int port;
 	private Thread listenThread;
 	private boolean listening = false;
+	private DatagramSocket socket;
 	
 	public Server(int port) {
 		this.port = port;
 	}
 	
 	public void start() {
+		try {
+			socket = new DatagramSocket(port);
+		} catch (SocketException e) {
+			e.printStackTrace();
+			return;
+		}
+		
 		listening = true;
 		
 		listenThread = new Thread(() -> listen());
@@ -29,7 +41,14 @@ public class Server {
 		
 	}
 	
-	public void send(byte[] data) { 
+	public void send(byte[] data, InetAddress address, int port) { 
+		assert(socket.isConnected());
+		DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+		try {
+			socket.send(packet);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 		
